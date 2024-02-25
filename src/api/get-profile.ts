4c5616ae43d87df86580d16a06884c59e5eebfcb
RafaelDomingues-R2D2 <rafael.dominguesr2d2@gmai.com>
2024-02-25 20:00:43 -0300

    setup

---
+++ src/api/get-profile.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+interface getProfileResponse {
+  id: string
+  name: string
+  email: string
+  phone: string | null
+  role: 'admin' | 'customer'
+  createAt: Date | null
+  updateAt: Date | null
+}
+
+export async function getProfile() {
+  const response = await api.get<getProfileResponse>('/me')
+
+  return response.data
+}
---
+++ src/components/account-menu.tsx
@@ -0,0 +1,78 @@
+import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { ChevronDown, LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
+
+import { Button } from './ui/button'
+import { Dialog } from './ui/dialog'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
+
+export function AccountMenu() {
+  const navigate = useNavigate()
+
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+    staleTime: Infinity,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
+  })
+
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            asChild
+            className="text-rose-500 dark:text-rose-400"
+            disabled={isSigningOut}
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  )
+}
